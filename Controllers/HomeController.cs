eac7c213f7a330b525ce41f3f9fad5c41f0bd8be
TuyMove <veeravat@me.com>
2021-11-23 16:12:11 +0700

    Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using getUserIP.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace getUserIP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration Configuration;
+
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.clientIP = HttpContext.Request.HttpContext.Connection.RemoteIpAddress;
+            var TM = new TuyMoveLib.Class1();
+            ViewBag.MyText = TM.GetText();
+            ViewBag.Header = HttpContext.Request.Headers;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace getUserIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
